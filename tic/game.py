2a05c19942635973a8973d95c2491ded23aacdcb
Shohei Miyashita <shohei.miyashita@kamasu.jp>
2017-02-22 17:02:42 +0900

    impl: run

--- tic/game.py
+++ tic/game.py
@@ -1,9 +1,9 @@
 def vaildate_action(board, x, y):
-    if is_empty(board, x, y)
+    if not is_empty(board, x, y):
         raise RuntimeError('there is a already action')
 
 def is_empty(board, x, y):
-    return board[x][y] is not None:
+    return board[x][y] is None
 
 LINES = [
     ((0, 0), (0, 1), (0, 2)),
@@ -33,29 +33,43 @@ def is_finished(board):
 class Game(object):
     def __init__(self, player_1, player_2):
         self.__board = [[None for _ in range(3)] for _ in range(3)]
-        self.__player_1
-        self.__player_2
+        self.__player_1 = player_1
+        self.__player_2 = player_2
         self.__turn = self.turn_enumerator()
         self.__winner = None
+
+    def is_filled(self):
+        for row in self.__board:
+            for point in row:
+                if point is None:
+                    return False
+        return True
 
     @property
     def winner(self):
         return self.__winner
 
     def next(self):
-        if not self.is_playing():
+        if self.__winner is not None:
+            return False
+
+        if self.is_filled():
             return False
 
         player = next(self.__turn)
-        action = player.select_action()
-        x, y = action
+
         while True:
             try:
+                action = player.select_action()
+                x, y = action
                 vaildate_action(self.__board, x, y)
-            except Exception e:
+                break
+            except Exception as e:
                 if e is KeyboardInterrupt:
                     exit()
                 continue
+
+        self.__board[x][y] = player
 
         if is_finished(self.__board):
             self.__winner = player
@@ -73,6 +87,7 @@ class Game(object):
                 elif point is self.__player_2:
                     row_str += 'x'
             print(row_str)
+        print('\n')
 
     def turn_enumerator(self):
         while True:
